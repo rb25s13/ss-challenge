8a39003cebea44e2d895c1184c6c04af3c2fef77
rb25s13 <rb25s13.dwc@gmail.com>
2022-07-21 09:09:03 -0500

    getResults func - added prev next buttons

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,9 @@
 ////// js by dillon carter
 
+// hide next and prev buttons as default
+$('button#prev-btn').css('display','none')
+$('button#next-btn').css('display','none')
+
 // build url using query value and page number
 buildUrl = (q, page) => {
     const urlWithParams = new URL('http://api.searchspring.net/api/search/search.json')
@@ -32,41 +36,64 @@ pageCheck = (data) => {
         prevPage: curPage-1,
         nextPage: curPage+1
     }
-    console.log(data.pagination)
     return pageObj
 }
 
+getResults = (data) => {
+    console.log('get results func', data)
+    console.log('page check func', pageCheck(data))
+    $('#item-results').html('')
+    $('#item-results').append(`
+        <h3 id="results" href="#results"></h3><br>
+        <div class="col-lg-12 col-md-12" id="items-container">
+        <div class="text-center" id="page-num"></div>
+        <div class="text-right" id="prevNextBtns">
+            <button class="btn btn-prime prev-btn" id="prev-btn">Previous</button>
+            <button class="btn btn-prime next-btn" id="next-btn">Next</button>
+        </div>
+        <br>
+        <div class="row" id="items-row"></div>
+        <br>
+        <div class="text-right" id="prevNextBtns">
+            <button class="btn btn-prime prev-btn" id="prev-btn">Previous</button>
+            <button class="btn btn-prime next-btn" id="next-btn">Next</button>
+        </div>
+        </div>
+    `)
+    pageCheck(data)
+
+    // display 'no results' if there are none
+    if (data.pagination.totalResults == 0) {
+        // $('#results').html("")
+        // $('#items-container').html("")
+        $('button#prev-btn').css('display','none')
+        $('button#next-btn').css('display','none')
+        $('#page-num').css('display', 'none')
+        $('#results').html(
+            `
+            <h3 id="results">No results for ${data.breadcrumbs[0].filterValue}<h3>
+            `
+        )
+        return
+    } 
+    else { 
+        $('#page-num').css('display', '')
+    }
+
+}
+
 // testing url fetch
-let testUrl = buildUrl('jeans',2)
+let testUrl = buildUrl('jeans',1)
 
 fetch(testUrl)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data)
-        console.log(pageCheck(data))
+        // console.log(data)
+        getResults(data)
     })
 
 
 
+    
+    
 
-// testing prev, next buttons
-$('#item-results').append(`
-    <h3 id="results" href="#results"></h3><br>
-    <div class="col-lg-12 col-md-12" id="items-container">
-    <div class="text-center" id="page-num"></div>
-    <div class="text-right" id="prevNextBtns">
-        <button class="btn btn-prime prev-btn" id="prev-btn">Previous</button>
-        <button class="btn btn-prime next-btn" id="next-btn">Next</button>
-    </div>
-    <br>
-    <div class="row" id="items-row"></div>
-    <br>
-    <div class="text-right" id="prevNextBtns">
-        <button class="btn btn-prime prev-btn" id="prev-btn">Previous</button>
-        <button class="btn btn-prime next-btn" id="next-btn">Next</button>
-    </div>
-    </div>
-`)
-// hide next and prev buttons as default
-$('button#prev-btn').css('display','none')
-$('button#next-btn').css('display','none')

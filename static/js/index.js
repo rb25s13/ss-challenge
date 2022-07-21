15aa9a74c73ed0aa026c73c1291e952f62ad1c64
rb25s13 <rb25s13.dwc@gmail.com>
2022-07-20 23:25:03 -0500

    buildUrl function using searchParams

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,17 @@
-let testUrl = 'http://api.searchspring.net/api/search/search.json?siteId=scmq7n&q=jeans&resultsFormat=native&page=2'
+////// js by dillon carter
+
+// build url using query value and page number
+buildUrl = (q, page) => {
+    const urlWithParams = new URL('http://api.searchspring.net/api/search/search.json')
+    urlWithParams.searchParams.append('siteId', 'scmq7n')
+    urlWithParams.searchParams.append('q', q)
+    urlWithParams.searchParams.append('resultsFormat', 'native')
+    urlWithParams.searchParams.append('page', page)
+    console.log('link built', urlWithParams.href)
+    return urlWithParams.href
+}
+
+let testUrl = buildUrl('jeans',1)
 
 fetch(testUrl)
     .then((response) => response.json())

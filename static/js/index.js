fb8b85724fd7ab5945358fe01ce45294c0ae7dc5
rb25s13 <rb25s13.dwc@gmail.com>
2022-07-20 23:34:35 -0500

    testing next, prev buttons

--- static/js/index.js
+++ static/js/index.js
@@ -11,8 +11,32 @@ buildUrl = (q, page) => {
     return urlWithParams.href
 }
 
+// testing url fetch
 let testUrl = buildUrl('jeans',1)
 
 fetch(testUrl)
     .then((response) => response.json())
     .then((data) => console.log(data))
+
+
+// testing prev, next buttons
+$('#item-results').append(`
+    <h3 id="results" href="#results"></h3><br>
+    <div class="col-lg-12 col-md-12" id="items-container">
+    <div class="text-center" id="page-num"></div>
+    <div class="text-right" id="prevNextBtns">
+        <button class="btn btn-prime prev-btn" id="prev-btn">Previous</button>
+        <button class="btn btn-prime next-btn" id="next-btn">Next</button>
+    </div>
+    <br>
+    <div class="row" id="items-row"></div>
+    <br>
+    <div class="text-right" id="prevNextBtns">
+        <button class="btn btn-prime prev-btn" id="prev-btn">Previous</button>
+        <button class="btn btn-prime next-btn" id="next-btn">Next</button>
+    </div>
+    </div>
+`)
+// hide next and prev buttons as default
+$('button#prev-btn').css('display','none')
+$('button#next-btn').css('display','none')
